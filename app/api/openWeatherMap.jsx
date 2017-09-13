44c842806d5d02b3c1d8798bea2addcc101d5085
Kristen Goehringer <kgoehringer@gmail.com>
2017-09-13 16:29:43 +0200

    initial build from boilerplate

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,21 @@
+// var axios = require('axios');
+
+// const OPEN_WEATHER_MAP_URL = "http://api.openweathermap.org/data/2.5/weather?appid=cba850684846574ad4b3e39cab2f91bb&units=imperial"
+
+// module.exports = {
+// 	getTemp: function(location){
+// 		var encodedLocation = encodeURIComponent(location);
+// 		var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+// 		return axios.get(requestUrl).then((res) => {
+// 			if(res.data.cod && res.data.message) {
+// 				throw new Error(res.data.message);
+// 			} else {
+// 				return res.data.main.temp;
+// 			}
+// 		}, (err) => {
+// 			throw new Error('Unable to fetch weather for that location.');
+// 		})
+// 	}
+// }
+// cba850684846574ad4b3e39cab2f91bb
